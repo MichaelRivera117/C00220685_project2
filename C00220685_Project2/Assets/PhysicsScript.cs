86f683288d237e3299bc876ba7a8ea1740e936a1
MichaelRivera117 <michaelrj1998@gmail.com>
2020-09-14 13:03:36 -0500

    fixed scripts
    step 3 scripts are now functions and not 6 different scripts

---
+++ C00220685_Project2/Assets/PhysicsScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+public class PhysicsScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("On trigger enter");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("On trigger stay");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("On trigger exit");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("On collision enter");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("On collision stay");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("On collision exit");
+    }
+}
